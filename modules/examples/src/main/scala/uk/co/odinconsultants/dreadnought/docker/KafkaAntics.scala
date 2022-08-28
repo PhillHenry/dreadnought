847c74196afe3a26da59c24f7f03dbfc03a7b6ad
PhillHenry@gmail.com <PhillHenry@gmail.com>
2022-08-28 11:44:12 +0100

    Buffers but does not appear to send. Hmm.

--- modules/examples/src/main/scala/uk/co/odinconsultants/dreadnought/docker/KafkaAntics.scala
+++ modules/examples/src/main/scala/uk/co/odinconsultants/dreadnought/docker/KafkaAntics.scala
@@ -2,8 +2,7 @@ package uk.co.odinconsultants.dreadnought.docker
 
 import cats.effect.{IO, IOApp}
 import com.comcast.ip4s.*
-import fs2.kafka.ProducerSettings
-import fs2.kafka.*
+import fs2.kafka.{ConsumerSettings, ProducerSettings, *}
 import fs2.Stream
 
 import scala.concurrent.duration.*
@@ -14,29 +13,55 @@ object KafkaAntics extends IOApp.Simple {
       address: IpAddress,
       port:    Port,
       topic:   String = "test_topic",
-  ): Stream[IO, Int] = {
+  ): Stream[IO, Unit] = {
+    val bootstrapServer                                        = s"localhost:${port.value}"
     val producerSettings: ProducerSettings[IO, String, String] =
       ProducerSettings[IO, String, String]
-        .withBootstrapServers(s"$address:${port.value}")
+        .withBootstrapServers(bootstrapServer)
 
-    produce(producerSettings, topic)
+    val consumerSettings =
+      ConsumerSettings[IO, String, String]
+        .withAutoOffsetReset(AutoOffsetReset.Earliest)
+        .withBootstrapServers(bootstrapServer)
+        .withGroupId("group")
+
+    consume(consumerSettings, topic).concurrently(produce(producerSettings, topic))
+//    produce(producerSettings, topic).concurrently(consume(consumerSettings, topic))
   }
+
+  def consume(
+      consumerSettings: ConsumerSettings[IO, String, String],
+      topic:            String,
+  ): Stream[IO, Unit] =
+    KafkaConsumer
+      .stream(consumerSettings)
+      .subscribeTo(topic)
+      .records
+      .evalMap { committable =>
+        val record = committable.record
+        IO.println(s"Consumed ${record.key} -> ${record.value}")
+      }
 
   def produce(
       producerSettings: ProducerSettings[IO, String, String],
       topic:            String,
-  ): Stream[IO, Int] =
-    KafkaProducer.stream(producerSettings).flatMap { producer =>
-      Stream
-        .emits(List("a", "b", "c").zipWithIndex)
-        .map((x, i) => i -> ProducerRecords.one(ProducerRecord(topic, s"key_$x", s"val_$x")))
-        .evalMap { case (offset, producerRecord) =>
-          producer
-            .produce(producerRecord)
-            .map(_.as(offset))
-        }
-        .parEvalMap(Int.MaxValue)(identity)
-    }
+  ) =
+    KafkaProducer
+      .stream(producerSettings)
+      .flatMap { producer =>
+        val s: Stream[IO, IO[ProducerResult[Unit, String, String]]] = Stream
+          .emits(List("a", "b", "c", "d").zipWithIndex)
+          .map((x, i) => i -> ProducerRecords.one(ProducerRecord(topic, s"key_$x", s"val_$x")))
+          .evalMap { case (offset, producerRecord) =>
+            val product: IO[IO[ProducerResult[Unit, String, String]]] =
+              producer.produce(producerRecord)
+            IO.println(s"buffering $producerRecord, offset = $offset") *> product
+          }
+        s // .evalMap(_.flatMap(IO.println))
+      }
+//      .groupWithin(500, 5.seconds)
+      .evalMap(_.flatMap(x => IO.println(s"Sending $x")))
+//      .through(commitBatchWithin(500, 15.seconds))
 
   def run: IO[Unit] = produceMessages(ip"127.0.0.1", port"9092").compile.drain
 
